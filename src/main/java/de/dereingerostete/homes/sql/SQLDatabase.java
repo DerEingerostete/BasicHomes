c0a66ca056908d1800fe709b7e7cf3abcf683de6
DerEingerostete <contact@dereingerostete.dev>
2023-01-23 17:47:17 +0100

    Update license and add license headers

--- src/main/java/de/dereingerostete/homes/sql/SQLDatabase.java
+++ src/main/java/de/dereingerostete/homes/sql/SQLDatabase.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023 × DerEingerostete
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package de.dereingerostete.homes.sql;
 
 import lombok.RequiredArgsConstructor;
